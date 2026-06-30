cf9209dba0a8ef3c4ea24c6d6258a1b8078296a8
Cida Luna <cidalunna@hotmail.com>
2026-06-29 22:29:26 -0300

    feat: start java main

---
+++ exercicios/fundamentos-java/src/main/java/br/com/cidaluna/Main.java
@@ -0,0 +1,7 @@
+package main.java.br.com.cidaluna;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá, Cida! Projeto fundamentos-java iniciado.");
+    }
+}
