3edbcc9bf908c23d419bf01f8d2938f3ccd2803a
Cida Luna <cidalunna@hotmail.com>
2026-07-19 15:37:28 -0300

    feat: add Corolla average price calculation to Veiculo stream example

--- exercicios/fundamentos-java/src/main/java/br/com/cidaluna/streams/ExemploStreamVeiculo.java
+++ exercicios/fundamentos-java/src/main/java/br/com/cidaluna/streams/ExemploStreamVeiculo.java
@@ -1,8 +1,7 @@
 package main.java.br.com.cidaluna.streams;
 
-
 import main.java.br.com.cidaluna.modelo.Veiculo;
-
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,7 +12,8 @@ public class ExemploStreamVeiculo {
 
         List<Veiculo> veiculos = List.of(
                 new Veiculo("Civic", "Honda", "Preto", 145000.00, 2023, 210),
-                new Veiculo("Corolla", "Toyota", "Branco", 140000.00, 2022, 220),
+                new Veiculo("Corolla", "Toyota", "Branco", 150000.00, 2022, 220),
+                new Veiculo("Corolla", "Toyota", "Prata", 140000.00, 2020, 210),
                 new Veiculo("Gol", "Volks", "Azul", 20000.00, 2020, 170),
                 new Veiculo("Golf", "Volks", "Preto", 200000.00, 2020, 220),
                 new Veiculo("X1", "BMW", "Branco", 300000.00, 2023, 250),
@@ -51,6 +51,28 @@ public class ExemploStreamVeiculo {
                 .average()
                 .orElse(0);
         System.out.println("Preço médio: R$" + String.format("%.2f", precoMedio));
+
+        // carro mais caro
+        Veiculo carroMaisCaro = veiculos.stream()
+                .max(Comparator.comparingDouble(Veiculo::getPreco))
+                .orElse(null); // ou um Veiculo "vazio" padrão, se preferir não lidar com null depois
+
+        if (carroMaisCaro != null) {
+            System.out.println("Carro mais caro custa: R$"
+                    + String.format("%.2f", carroMaisCaro.getPreco())
+                    + " é " + carroMaisCaro.getNome());
+        }
+
+        // media dos Corollas
+        double mediaCorollas = veiculos
+                .stream()
+                .filter(v -> v.getNome().equalsIgnoreCase("corolla"))
+                .mapToDouble(Veiculo::getPreco)
+                .average()
+                .orElse(0);
+
+        System.out.println("Média de preço dos Corollas: R$" + String.format("%.2f", mediaCorollas));
+
 
         // groupingBy: agrupa os veículos por marca
         System.out.println("\n--- Agrupados por marca ---");
