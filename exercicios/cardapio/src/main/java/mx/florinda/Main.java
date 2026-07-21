a82661b951c3b39bac4030bd7575403a9e8e0456
Cida Luna <cidalunna@hotmail.com>
2026-07-20 22:22:38 -0300

    feat: add list and stream

--- exercicios/cardapio/src/main/java/mx/florinda/Main.java
+++ exercicios/cardapio/src/main/java/mx/florinda/Main.java
@@ -1,7 +1,13 @@
 package mx.florinda;
 
 import mx.florinda.cardapio.ItemCardapio;
+import mx.florinda.enums.CategoriaCardapio;
+
+import java.sql.SQLOutput;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,8 +15,20 @@ public class Main {
         Database database = new Database();
         List<ItemCardapio> itens = database.listaDeItensCardapio();
 
+        // Saber quais as categorias realmente tenho no cardapio
+        Set<CategoriaCardapio> categorias = new HashSet<>();
         for(ItemCardapio item: itens){
-            System.out.println(item);
+            CategoriaCardapio categoria = item.categoria();
+            categorias.add(categoria);
         }
+        for(CategoriaCardapio categoria : categorias){
+            System.out.println(categoria);
+        }
+
+        System.out.println("----> Stream");
+        itens.stream()
+                .map(ItemCardapio::categoria)
+                .collect(Collectors.toSet())
+                .forEach(System.out::println);
     }
 }
