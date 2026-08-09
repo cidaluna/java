ea26a61ce242380baccec693a9e8af4552dcee0d
Cida Luna <cidalunna@hotmail.com>
2026-08-09 14:13:10 -0300

    feat: get data json by http client

--- exercicios/cardapio/src/main/java/mx/florinda/ClienteViaCEP.java
+++ exercicios/cardapio/src/main/java/mx/florinda/ClienteViaCEP.java
@@ -1,18 +1,25 @@
 package mx.florinda;
 
-import java.net.URL;
-import java.util.Scanner;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ClienteViaCEP {
 
     public static void main(String[] args) throws Exception {
-        URL url = new URL("https://viacep.com.br/ws/01001000/json/");
+        // URL url = new URL("https://viacep.com.br/ws/01001000/json/");
 
-        try (Scanner scanner = new Scanner(url.openStream())) { // nesse caso ele mesmo fecha o scanner
-            while(scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                System.out.println(line);
-            }
+        URI uri = URI.create("https://viacep.com.br/ws/01001000/json/");
+
+        try(HttpClient httpClient = HttpClient.newHttpClient()) {
+            HttpRequest httpRequest = HttpRequest.newBuilder(uri).build();
+            HttpResponse<String> httpResponse = httpClient.send(httpRequest,
+                    HttpResponse.BodyHandlers.ofString());
+            int statusCode = httpResponse.statusCode();
+            String body = httpResponse.body();
+            System.out.println("\nStatus Code: " +statusCode);
+            System.out.println("Body: " +body);
         }
 
     }
